d0cc95c3c1dfb9b8304abe5dc18aebf2e501df1c
dimashnureke <103212563+dimashnureke@users.noreply.github.com>
2022-04-25 01:35:52 +0600

    Initial commit

---
+++ project/src/Header.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header>
+          
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ project/src/Main.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <img src="https://resource.logitechg.com/w_1800,c_limit,f_auto,q_auto,f_auto,dpr_auto/d_transparent.gif/content/dam/gaming/en/homepage/league-of-legends-hp-banner-desktop-new.png?v=1" alt="photo" width="100%"></img>
+    </div>
+  );
+}
+
+export default Main;
+
